cb5e69511ff92614bba26e1827633d304222ef5c
Candace  Massman <candacem@Meme-Dealer.local>
2019-02-26 20:36:05 -0600

    Applying current build

---
+++ DocumentMerger/DocumentMerger/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace DocumentMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ~~~~~ Document Merger ~~~~~ ");
+            Console.WriteLine();
+
+            string FirstFile;
+            string SecondFile;
+
+            do
+            { //ask user for file names & verifies if they exist
+
+                Console.WriteLine("Name of first file: ");
+                  FirstFile = GetInput();
+                Console.WriteLine("Name second file: ");
+                  SecondFile = GetInput();
+
+                string NewFileName = FirstFile.Substring(0, 4) + SecondFile.Substring(0, 4) + ".txt"; //combines file names, removes .txt from first/second files and adds it to end of combined name
+                string NewFileText = FirstFile + SecondFile; // combines file text
+
+                NewFile(NewFileName, NewFileText);
+
+                //display that file was saved if no exception
+
+           Console.WriteLine("{0} was successfully saved. The document contains {1} characters", NewFileName, NewFileText);
+
+                //ask if user wants to merge two more
+
+              Console.WriteLine("Would you like to merge any more? (y/n): ");
+
+            } while (Console.ReadLine().ToLower().Equals("y"));
+        }
+
+        static string GetInput()
+        {
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+                if (Check(input)) return input;
+                Console.WriteLine("This file does not exist. Please try again.");
+
+            } while (true);
+        }
+
+        static bool Check(string input)
+        {
+            if (File.Exists(input)) return false;
+
+            return true;
+
+        }
+
+        static void NewFile(string content, string name)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(name);
+
+                sw.WriteLine(content);
+
+                sw.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Process failed: " + e.Message); //gives exceptions
+                Environment.Exit(1);
+            }
+        }
+    }
+}
